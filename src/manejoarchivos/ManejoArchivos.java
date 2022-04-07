c31e2eff104f78773b4dc8214fa25f6dda1ca920
Didier Ramirez <didierjavierr@gmail.com>
2022-04-07 11:03:46 -0500

    El archivo se crea de forma correcta

--- src/manejoarchivos/ManejoArchivos.java
+++ src/manejoarchivos/ManejoArchivos.java
@@ -4,10 +4,23 @@
  */
 package manejoarchivos;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 /**
  *
  * @author DIDIER JAVIER
  */
 public class ManejoArchivos {
     
+    public static void crearArchivo(String nombreArchivo){
+        File archivo = new File(nombreArchivo);//Se crea un objeto de tipo file en memoria
+        try {
+            PrintWriter salida = new PrintWriter(archivo);//PrintWriter permite crear y escribir archivos
+            salida.close();//El archivo que se abrió se tiene que cerrar y en este momento se crea el archivo en memoria
+            System.out.println("Se ha creado el archivo");
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace(System.out);
+        }
+    }
 }
--- src/test/TestManejoArchivos.java
+++ src/test/TestManejoArchivos.java
@@ -4,10 +4,16 @@
  */
 package test;
 
+import static manejoarchivos.ManejoArchivos.crearArchivo;
+
 /**
  *
  * @author DIDIER JAVIER
  */
 public class TestManejoArchivos {
-    
+    public static void main(String[] args) {
+        
+        var nombreArchivo = "Prueba.txt";
+        crearArchivo(nombreArchivo);
+    }
 }
